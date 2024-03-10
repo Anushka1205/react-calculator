01c1d618dc15eb13700022c5e4c2ab2baf1e3248
Anushka1205 <anushka.bhatt@kalvium.community>
2024-03-10 21:15:59 +0530

    commit changes

--- calculator/src/component/cal.jsx
+++ calculator/src/component/cal.jsx
@@ -1,33 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css'
 
-function Cal() {
+export default function Cal() {
+  const [inputvalue, setInput]= useState("")
+  const [outputvalue, setOutput]= useState("")
+  const handleClick = (value) => {
+    if (value === '=') {
+      setOutput(eval(inputvalue));
+      setInput("")
+    } else if (value === 'DEL') {
+      setInput('');
+      setOutput('');
+    } else {
+      if(outputvalue !=""){
+        setOutput("")
+      }
+      setInput(inputvalue + value);
+    }
+  };
 
   return (
     <div className='body'>
         <div className='outer'>
-            <div className='box1' style={{backgroundColor:"#bec6da"}} ></div>
-            <div className='box2'>AC</div>
-            <div>DEL</div>
-            <div>%</div>
-            <div>2</div>
-            <div>1</div>
-            <div>3</div>
-            <div>*</div>
-            <div>4</div>
-            <div>5</div>
-            <div>6</div>
-            <div>+</div>
-            <div>7</div>
-            <div>8</div>
-            <div>9</div>
-            <div>-</div>
-            <div>.</div>
-            <div>0</div>
-            <div className='equalto'>=</div>
+            <div className='box1' style={{backgroundColor:"#c5d1e8"}} > {inputvalue} {outputvalue} </div>
+            <div className='box2' onClick={()=>handleClick('AC')}>AC</div>
+            <div onClick={()=>handleClick('DEL')}>DEL</div>
+            <div onClick={()=>handleClick('%')}>%</div>
+            <div onClick={()=>handleClick('2')}>2</div>
+            <div onClick={()=>handleClick('1')}>1</div>
+            <div onClick={()=>handleClick('3')}>3</div>
+            <div onClick={()=>handleClick('*')}>*</div>
+            <div onClick={()=>handleClick('4')}>4</div>
+            <div onClick={()=>handleClick('5')}>5</div>
+            <div onClick={()=>handleClick('6')}>6</div>
+            <div onClick={()=>handleClick('+')}>+</div>
+            <div onClick={()=>handleClick('7')}>7</div>
+            <div onClick={()=>handleClick('8')}>8</div>
+            <div onClick={()=>handleClick('9')}>9</div>
+            <div onClick={()=>handleClick('-')}>-</div>
+            <div onClick={()=>handleClick('.')}>.</div>
+            <div onClick={()=>handleClick('0')}>0</div>
+            <div onClick={()=>handleClick('=')} className='equalto'>=</div>
         </div>
     </div>
   )
 }
-
-export default Cal
